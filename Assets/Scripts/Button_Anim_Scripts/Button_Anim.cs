4b9479fe01b9b94e6085738d4ddeb27561174576
terraplus12 <oganesszivert@gmail.com>
2025-07-28 17:04:08 +0300

    1

---
+++ Assets/Scripts/Button_Anim_Scripts/Button_Anim.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using Unity.VisualScripting;
+
+public abstract class Button_Anim : MonoBehaviour, IPointerDownHandler
+{
+    protected Vector3 Original_Scale;
+    [SerializeField] protected float Scale_Factor = 0.6f;
+    [SerializeField] protected float Anim_Time = 0.2f;
+    private Tween Anim;
+
+    public delegate void Click_Event();
+    public event Click_Event click;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (Anim != null && Anim.IsActive() && Anim.IsPlaying())
+        {
+            return;
+        }
+        Anim = transform.DOScale(Original_Scale * Scale_Factor, Anim_Time);
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (Anim != null && Anim.IsActive() && Anim.IsPlaying())
+        {
+            Anim.Kill();
+        }
+
+        Anim = transform.DOScale(Original_Scale, Anim_Time);
+    }
+    void Start()
+    {
+        Original_Scale = transform.localScale;
+    }
+    protected abstract void OnButtonClicked();
+    void Update()
+    {
+
+    }
+}
+    
+   
---
+++ Assets/Scripts/Button_Anim_Scripts/Button_Play_Anim.cs
@@ -0,0 +1,18 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Button_Play_Anim : Button_Anim
+{
+    protected override void OnButtonClicked()
+    {
+        
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
